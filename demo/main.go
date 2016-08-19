6a77467c8eb362ac78daaa472d81d26693df397d
Alexander Garin <garin1221w@gmail.com>
2016-08-19 23:16:44 +0500

    - Первая стабильная версия шаблонизатора

--- demo/main.go
+++ demo/main.go
@@ -1,10 +1,37 @@
 package main
 
 import (
+    "fmt"
     "github.com/legion-zver/go-xlsx-templates"
 )
 
-func main()  {    
-    xlsxt.OpenTemplate("export.xlsx")
+type SubItem struct {
+    Name string
+}
 
+type Item struct {
+    Name string
+    SubItems []SubItem
+}
+
+type Test struct {
+    Items []Item
+}
+
+func main()  {    
+    file, err := xlsxt.OpenTemplate("example.xlsx")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    test := Test{Items: []Item{
+        Item{Name: "item1", SubItems: []SubItem{ SubItem{ Name: "1"}, SubItem{ Name: "2"}}},
+        Item{Name: "item2", SubItems: []SubItem{ SubItem{ Name: "1"}, SubItem{ Name: "2"}, SubItem{ Name: "3"}}}}}
+
+    err = file.RenderTemplate(&test)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    file.Save("result.xlsx")   
 }
--- xlsxt.go
+++ xlsxt.go
@@ -1,20 +1,20 @@
 package xlsxt
 
 import (
-    "io"
-    //"fmt"
+    "io"    
+    "fmt"
     "errors"
     "regexp"
     "reflect"
-    "strings"
-    "strconv"
+    "strings"   
+    "strconv" 
     "github.com/tealeg/xlsx"
     "github.com/aymerick/raymond"
 )
 
 var (
     rxTemplateItem  = regexp.MustCompile(`\{\{\s*([\w|\.]+)\s*\}\}`)
-	rxMergeCellV    = regexp.MustCompile(`\[\s?v-merge:\s?(\d+)\s?\]`)
+	rxMergeCellV    = regexp.MustCompile(`\[\s?v-merge\s?\]`)
 )
 
 
@@ -80,26 +80,185 @@ func (s *XlsxTemplateFile) RenderTemplate(v interface{}) error {
             cloneSheet(sheet, newSheet)
             // Получаем объект
             obj := getObject(v, sheetIndex)
+            // Раскладываем объект на граф
+            graph := new(node)
+            graph.FromObject(v)
+            // Вывод для теста
+            lines := graph.ListMap()            
             // Проходимся по строкам
             for _, row := range sheet.Rows {
                 // Проверка на массив или срез
                 if !haveArrayInRow(row, obj) {                    
-                    newRow := newSheet.AddRow()
-                    cloneRow(row, newRow)
-                    err := renderRow(newRow, obj)
-                    if err != nil {
+                    newRow := newSheet.AddRow(); cloneRow(row, newRow)
+                    if err := renderRow(newRow, obj); err != nil {
                         return nil
                     }
                     continue
                 }
-                // Если это массив или срез
-            }            
+                for i := 0; i < len(lines); i++ {
+                    newRow := newSheet.AddRow()
+                    cloneRow(row, newRow)                    
+                    if err := renderRow(newRow, lines[i]); err != nil {
+                        return nil
+                    }
+                }
+            }
+            graph = nil            
         }
+        return nil
     }
     return errors.New("Not load template xlsx file")
 }
 
 /* Вспомогательные функции */
+
+type cacheMergeCell struct {
+
+}
+
+// node - элмент графа
+type node struct {
+    name     string
+    values   map[string]interface{}    
+    nodes []*node
+}
+
+func (n *node) String() string {    
+    return fmt.Sprintln(n.ListMap()) 
+}
+
+func (n *node) ListMap() []map[string]interface{} {
+    var m = make([]map[string]interface{}, 0)
+    n.toListMap("", make(map[string]interface{}), &m)
+    return m
+} 
+
+func (n *node) toListMap(name string, current map[string]interface{}, out *[]map[string]interface{}) {
+    for key, value := range n.values {
+        if len(n.name) > 0 {
+            key = n.name + "_" + key
+        } 
+        if len(name) > 0 {
+            key = name + "_" + key
+        }
+        current[key] = value
+    }
+    if len(n.nodes) > 0 {
+        path := name
+        if len(n.name) > 0 {
+            if len(path) > 0 {
+                path += "_"
+            } 
+            path += n.name
+        }
+        // Проходимся и формируем счетчик
+        var counter = make(map[string]int)        
+        for _, node := range n.nodes {
+            nname := path
+            if len(nname) > 0 {
+                if len(node.name) > 0 {
+                    nname += "_"
+                }                 
+            }
+            nname += node.name            
+            if cv, ok := counter[nname+"_length"]; ok {
+                counter[nname+"_length"] = cv + 1
+            } else { 
+                counter[nname+"_length"] = 1
+            }
+        }
+        for key, cv := range counter {
+            current[key] = cv
+        }
+        // Формируем строки
+        for _, node := range n.nodes {
+            var nw = make(map[string]interface{})
+            for k,v := range current {
+                nw[k] = v
+            }       
+            node.toListMap(path, nw, out)
+        }
+    } else {
+        *out = append(*out, current)
+    }
+}
+
+func (n* node) FromObject(obj interface{}) {
+    if n.values == nil {
+        n.values = make(map[string]interface{}, 0)
+    }
+    if n.nodes == nil {
+        n.nodes = make([]*node, 0)
+    }
+    val := reflect.ValueOf(obj)
+    kind := val.Type().Kind()
+    if kind == reflect.Ptr || kind == reflect.Interface {
+        val  = val.Elem()
+        kind = val.Type().Kind()
+    }
+    if kind == reflect.Struct {
+        t := val.Type()
+        for i := 0; i < t.NumField(); i++ {
+            field := t.Field(i)
+            fv := val.FieldByIndex(field.Index)
+            kind = fv.Type().Kind()
+            if kind == reflect.Ptr || kind == reflect.Interface {
+                fv  = fv.Elem()
+                kind = fv.Type().Kind()
+            }
+            if kind == reflect.Map {
+                node := new(node)
+                node.name = field.Name
+                node.FromObject(fv.Interface())
+                n.nodes = append(n.nodes, node)
+            } else if kind == reflect.Array || kind == reflect.Slice {                
+                for j := 0; j < fv.Len(); j++ {
+                    node := new(node)
+                    node.name = field.Name
+                    node.FromObject(fv.Index(j).Interface())
+                    n.nodes = append(n.nodes, node)
+                }
+            } else {
+                n.values[field.Name] = val.FieldByIndex(field.Index).Interface()
+            }
+        }
+    } else if kind == reflect.Map {
+        for _, key := range val.MapKeys() {
+            mapItem := val.MapIndex(key)
+            kind = mapItem.Type().Kind()
+            if kind == reflect.Ptr || kind == reflect.Interface {
+                mapItem  = mapItem.Elem()
+                kind = mapItem.Type().Kind()
+            }
+            if kind == reflect.Map {
+                node := new(node)
+                node.name = key.String()
+                node.FromObject(mapItem.Interface())
+                n.nodes = append(n.nodes, node)
+            } else if kind == reflect.Array || kind == reflect.Slice {                
+                for j := 0; j < mapItem.Len(); j++ {
+                    node := new(node)
+                    node.name = key.String()
+                    node.FromObject(mapItem.Index(j).Interface())
+                    n.nodes = append(n.nodes, node)
+                }
+            } else {
+                n.values[key.String()] = mapItem.Interface()
+            }
+        }
+    } else if kind == reflect.Array || kind == reflect.Slice {
+        for i := 0; i < val.Len(); i++ {
+            node := new(node)
+            node.name = n.name
+            if len(node.name) > 0 {
+                node.name += "_"+strconv.FormatInt(int64(i),10)
+            }
+            node.FromObject(val.Index(i).Interface())
+            n.nodes = append(n.nodes, node)
+        }
+    }
+}
+
 
 func getObject(v interface{}, index int) interface{} {
     val := reflect.ValueOf(v)
@@ -147,26 +306,65 @@ func cloneSheet(from, to *xlsx.Sheet) {
 	}
 }
 
+func indexRow(row *xlsx.Row) int {
+    if row != nil && row.Sheet != nil {
+        for i, r := range row.Sheet.Rows {
+            if r == row {
+                return i
+            }
+        }
+    } 
+    return -1
+}
+
+func indexCell(cell *xlsx.Cell) int {
+    if cell != nil && cell.Row != nil {
+        for i, c := range cell.Row.Cells {
+            if c == cell {
+                return i
+            }
+        }
+    } 
+    return -1
+}
+
 // renderCell - рендер ячейки
 func renderCell(cell *xlsx.Cell, v interface{}) error {	    
-    template, err := raymond.Parse(cell.Value)
-	if err != nil {
-		return err
-	}
-    out, err := template.Exec(v)
+    // Правки для совместимости шаблонизатора
+    tpl := strings.Replace(cell.Value, "{{", "{{{", -1)
+	tpl = strings.Replace(tpl, "}}", "}}}", -1)
+    tpl = strings.Replace(tpl,".","_",-1)
+    tpl = strings.Replace(tpl,":length","_length",-1)
+    // Обработка контента
+    out, err := raymond.Render(tpl, v)
 	if err != nil {
 		return err
 	}
     cell.Value = out  
-    // После преобразования выполняем   
+    // Если у поля есть флаг авто мерджинга, то начинаем проверку   
     if rxMergeCellV.MatchString(cell.Value) {
-        vals := rxMergeCellV.FindStringSubmatch(cell.Value)
-        if len(vals) > 1 {
-            if val, err := strconv.Atoi(vals[1]); err == nil && val > 0 {
-                cell.VMerge = val
+        cell.Value = rxMergeCellV.ReplaceAllString(cell.Value, "")
+        if len(strings.TrimSpace(cell.Value)) > 0 {
+            // Проверяем значения
+            ic, ir := indexCell(cell), indexRow(cell.Row)
+            if ir >= 0 && ic >= 0 {
+                var lastRow *xlsx.Row
+                for i := (ir-1); i >= 0; i-- {
+                    row := cell.Row.Sheet.Rows[i]
+                    if row.Cells[ic].Value == cell.Value {
+                        lastRow = row
+                    } else {
+                        break
+                    }
+                }
+                if lastRow != cell.Row {
+                    ilr := indexRow(lastRow)
+                    if ilr >= 0 {
+                        lastRow.Cells[ic].VMerge = ir-ilr
+                    }
+                }
             }
         }
-        cell.Value = rxMergeCellV.ReplaceAllString(cell.Value, "")
     }
 	return nil
 }
@@ -192,7 +390,7 @@ func haveArrayInRow(row *xlsx.Row, v interface{}) bool {
                 if len(names) > 0 {
                     t := reflect.TypeOf(v)
                     for _, name := range names {
-                        t := findInType(t, name)
+                        t := findType(t, name)
                         if t != nil {
                             if t.Kind() == reflect.Array || t.Kind() == reflect.Slice {
                                 return true
@@ -208,17 +406,36 @@ func haveArrayInRow(row *xlsx.Row, v interface{}) bool {
     return false
 }
 
-// findInType - получаем тип по имени
-func findInType(t reflect.Type, name string) reflect.Type {
+
+// findType - получаем тип по имени
+func findType(t reflect.Type, name string) reflect.Type {
     kind := t.Kind()
     // Если это ссылка, то получаем истенный тип
     if kind == reflect.Ptr || kind == reflect.Interface {
         t = t.Elem()
     }
+    kind = t.Kind()
     if kind == reflect.Struct {
         if field, ok := t.FieldByName(name); ok {
             return field.Type
         }
     } 
     return nil
+}
+
+// findValue - получаем тип по имени
+func findValue(v reflect.Value, name string) (reflect.Value, bool) {
+    kind := v.Type().Kind()
+    // Если это ссылка, то получаем истенный тип
+    if kind == reflect.Ptr || kind == reflect.Interface {
+        v = v.Elem()
+    }
+    kind = v.Type().Kind()
+    if kind == reflect.Struct {
+        v := v.FieldByName(name)
+        if v.IsValid() {
+            return v, true
+        }        
+    } 
+    return v, false
 }
